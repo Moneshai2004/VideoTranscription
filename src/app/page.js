d2902e372ae9417aa3d38c5ca45b400210a50b2c
Moneshai2004 <moneshshanmugam@gmail.com>
2024-02-06 11:29:20 +0530

    home elements

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
 
-
 export default function Home() {
   return (
-
-  <div>test</div>
-
-  )
+    <>
+    <main className="p-4">
+       <header className="flex justify-between mb-4">
+        <a href="">Caption Generator</a>
+        <nav className="flex space-x-4">
+          <a href="">Home</a>
+          <a href="">pricing</a>
+          <a href="">contact</a>
+        </nav>
+      </header>
+      <div>
+        <h1>Add Caption to your videos</h1>
+        <h2>Just upload your videos and we will do rest</h2>
+      </div>
+      <div>
+        <button>Choose Files</button>
+      </div>
+    </main>
+     
+    </>
+  );
 }
