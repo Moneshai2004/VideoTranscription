cdf14c1b13355cd8b59084a4379f1d1b95faf9d2
Moneshai2004 <moneshshanmugam@gmail.com>
2024-02-09 10:34:33 +0530

    feature:button

---
+++ src/app/components/UploadFile.js
@@ -0,0 +1,16 @@
+import Uploadfileicon from "./UploadFileicon";
+
+export default function UploadFile(){
+    function upload(ev) {
+      ev.preventDefault();
+      console.log(ev);
+    }
+    return (
+
+      <label className="flex gap-2 rounded-full px-4 py-2 bg-orange-500 text-white ">
+        <Uploadfileicon />
+        Choose Files
+        <input onChange={upload} type="file" className="hidden"></input>
+      </label>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,21 @@
 import HeaderFiles from "./components/HeaderFile";
 import SectionBox from "./components/SectionBox";
 import Sparkleicon from "./components/Sparkleicon";
+import UploadFile from "./components/UploadFile";
 import Uploadfileicon from "./components/UploadFileicon";
 
 export default function Home() {
   return (
     <>
-     <HeaderFiles/>
+      <HeaderFiles />
       <div className="text-center mt-24">
         <h1 className="text-4xl">Add Caption to your videos</h1>
         <h2>Just upload your videos and we will do rest</h2>
       </div>
       <div className="flex justify-center mt-10">
-        <button className="flex gap-2 rounded-full px-4 py-2 bg-orange-500 text-white ">
-          <Uploadfileicon/>
-          Choose Files
-        </button>
+        <UploadFile />
       </div>
-     <SectionBox/>
+      <SectionBox />
     </>
   );
 }
