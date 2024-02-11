086a52063538f15b588c2500f4ea059537034656
Moneshai2004 <moneshshanmugam@gmail.com>
2024-02-11 21:04:15 +0530

    feature:axios

--- src/app/components/UploadFile.js
+++ src/app/components/UploadFile.js
@@ -5,9 +5,12 @@ export default function UploadFile(){
     function upload(ev) {
       ev.preventDefault();
       console.log(ev);
+      const files = ev.target.files;
+      if(files.length>0){
+        const files =files[0];
+      }
     }
     return (
-
       <label className="flex gap-2 rounded-full px-4 py-2 bg-orange-500 text-white ">
         <Uploadfileicon />
         Choose Files
